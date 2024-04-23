9abf5cb73f10df3ad7673721f7c6b1548426a8c1
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-04-23 18:08:58 +0200

    fix

--- variables.tf
+++ variables.tf
@@ -20,12 +20,6 @@ variable "delegations" {
   }
 }
 
-variable "aws_organizations_resource_policy_json" {
-  description = "JSON of the AWS Organizations Delegation. Ensure this is only specified in one instance of this module"
-  type        = string
-  default     = null
-}
-
 variable "aws_organizations_resource_policy" {
   description = "JSON of the AWS Organizations Delegation. Ensure this is only specified in one instance of this module"
   type        = object({
